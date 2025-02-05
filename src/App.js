0743ecc2ad8f053f4d84967e741437c59b6341d1
Shivam Soni <shivam.soni1998@gmail.com>
2025-02-05 21:24:30 +0530

    hooks examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import Component1 from "./useState/Component1";
+import Component2 from "./useRef/Component2";
+import Component3 from "./useEffect/Component3";
+
 function App() {
+  const [showComponent3, setShowComponent3] = useState(true);
   return (
-    <div style={{display:'flex',alignItems:"center",justifyContent:"center"}}>
-      <h1>Please Check Git Branches for Code</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Component1 />
+
+      <hr style={{ width: "100%", border: "1px solid black" }} />
+      <Component2 />
+      <hr style={{ width: "100%", border: "1px solid black" }} />
+      {showComponent3 && <Component3 />}
+      <br />
+      {/* Button to toggle Component3 */}
+      <button onClick={() => setShowComponent3(!showComponent3)}>
+        Toggle Component 3
+      </button>
     </div>
   );
 }
---
+++ src/useState/Component1.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Component1 = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <h1>{count}</h1>
+      </div>
+      <br />
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Component1;

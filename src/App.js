03e6b312d955a68e9df30a6b0492cf6fbe8dcc95
Shivam Soni <shivam.soni1998@gmail.com>
2025-02-05 21:01:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import  Component1  from "./useState/component1";
-import  Component2  from "./useRef/component2";
-import Component3 from "./useEffect/Component3";
-import { useState } from "react";
-
 function App() {
-  const [showComponent3, setShowComponent3] = useState(true);
   return (
-    <div className="App">
-   <Component1/>
-   <hr></hr>
-   <Component2/>
-   <hr></hr>
-   {showComponent3 && <Component3 />}
-      
-      {/* Button to toggle Component3 */}
-      <button onClick={() => setShowComponent3(!showComponent3)}>
-        Toggle Component 3
-      </button>
+    <div style={{display:'flex',alignItems:"center",justifyContent:"center"}}>
+      <h1>Please Check Git Branches for Code</h1>
     </div>
   );
 }
